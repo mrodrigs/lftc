96fbbf490c2404ad881f47a483cd15b773b7dd87
Mauricio Rodrigues <mauricio.rodrigues@gobots.com.br>
2021-01-09 22:24:58 -0300

    feat: load global styles and fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import {  useEffect } from 'react';
+import WebFont from 'webfontloader';
+
 import AppRoutes from './routes';
+import GlobalStyles from './styles/global';
 
 function App() {
+  function loadResources() {
+    WebFont.load({
+      google: {
+        families: [
+          'Montserrat:300,400,500,600,700,800,900',
+          'Nunito:300,400,500,600,700,800,900',
+        ],
+      },
+    });
+  }
+
+  useEffect(() => {
+    loadResources();
+  }, []);
+
   return (
     <>
+      <GlobalStyles />
       <AppRoutes />
     </>
   );
