efd627a44577733607e60487e11c69005b503cf2
Mauricio Rodrigues <mauricio.rodrigues@gobots.com.br>
2021-01-09 21:04:26 -0300

    feat: use app routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import AppRoutes from './routes';
+
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <>
+      <AppRoutes />
+    </>
   );
 }
 
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
