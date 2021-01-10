635d773c590517d27e466a6c0180d1217e515b29
Mauricio Rodrigues <mauricio.rodrigues@gobots.com.br>
2021-01-09 21:09:48 -0300

    feat: create regex route

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 
 import Home from './pages/Home';
+import Regex from './pages/Regex';
 
 export default function Routes() {
   return (
@@ -12,6 +13,9 @@ export default function Routes() {
       <Switch>
         <Route path="/" exact>
           <Home />
+        </Route>
+        <Route path="/regex" exact>
+          <Regex />
         </Route>
       </Switch>
     </Router>
